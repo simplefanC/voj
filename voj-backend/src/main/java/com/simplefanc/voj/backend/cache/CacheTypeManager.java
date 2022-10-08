f9a2d4fa4c198e7cfb56869c04f3b83b50b8fc6a
chenfan <simplefanc@gmail.com>
2022-10-08 11:37:01 +0800

    modify caffeine cache expire time

--- voj-backend/src/main/java/com/simplefanc/voj/backend/cache/CacheTypeManager.java
+++ voj-backend/src/main/java/com/simplefanc/voj/backend/cache/CacheTypeManager.java
@@ -13,11 +13,11 @@ import java.util.Map;
 public class CacheTypeManager {
     public static final Map<String, CacheType> CACHE_TYPE_MAP = new HashMap<>(){
         {
-            put(RedisConstant.OI_CONTEST_RANK_CACHE, new CacheType(RedisConstant.OI_CONTEST_RANK_CACHE, 2 * 3600, 2 * 3600));
-            put(RedisConstant.CONTEST_RANK_CAL_RESULT_CACHE, new CacheType(RedisConstant.CONTEST_RANK_CAL_RESULT_CACHE, 15, 15));
-            put(RedisConstant.SUPER_ADMIN_UID_LIST_CACHE, new CacheType(RedisConstant.SUPER_ADMIN_UID_LIST_CACHE, 12 * 3600, 12 * 3600));
-            put(RedisConstant.ACM_RANK_CACHE, new CacheType(RedisConstant.ACM_RANK_CACHE, 60, 60));
-            put(RedisConstant.OI_RANK_CACHE, new CacheType(RedisConstant.OI_RANK_CACHE, 60, 60));
+            put(RedisConstant.OI_CONTEST_RANK_CACHE, new CacheType(RedisConstant.OI_CONTEST_RANK_CACHE, 3600, 2 * 3600));
+            put(RedisConstant.CONTEST_RANK_CAL_RESULT_CACHE, new CacheType(RedisConstant.CONTEST_RANK_CAL_RESULT_CACHE, 8, 16));
+            put(RedisConstant.SUPER_ADMIN_UID_LIST_CACHE, new CacheType(RedisConstant.SUPER_ADMIN_UID_LIST_CACHE, 6 * 3600, 12 * 3600));
+            put(RedisConstant.ACM_RANK_CACHE, new CacheType(RedisConstant.ACM_RANK_CACHE, 30, 60));
+            put(RedisConstant.OI_RANK_CACHE, new CacheType(RedisConstant.OI_RANK_CACHE, 30, 60));
         }
     };
 
